ed70f5054b1722ccb0bee87dc0465e8db513af40
Weslley Higor <89808851+weslleyhigor@users.noreply.github.com>
2025-01-06 21:32:42 -0300

    add skills lowcode.js

--- src/javascript/skills.js
+++ src/javascript/skills.js
@@ -1,8 +1,8 @@
-import { skillsFrontend, skillsBackend } from "../database/data.js";
+import { skillsFrontend, skillsBackend, skillsLowCode } from "../database/data.js";
 
 const cardFrontend = document.querySelector('.skill__frontend__card__container')
 const cardBackend = document.querySelector('.skill__backend__card__container')
-
+const cardLowcode = document.querySelector('.skill__lowcode__container')
 
 export function createCardSkills () {
 
@@ -18,6 +18,15 @@ export function createCardSkills () {
     skillsBackend.forEach((element)=>{
         cardBackend.insertAdjacentHTML("beforeend", `
             <li class="skill__backend__card">
+                <img src="${element.logoUrl}">
+                <p>${element.name}</p>
+            </li>
+        `)
+    })
+
+    skillsLowCode.forEach((element)=>{
+        cardBackend.insertAdjacentHTML("beforeend", `
+            <li class="skill__lowcode__card">
                 <img src="${element.logoUrl}">
                 <p>${element.name}</p>
             </li>
