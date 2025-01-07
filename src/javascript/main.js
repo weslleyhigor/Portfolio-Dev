6ee54daba28c77819581dc36de3110e7f6b1ddc5
Weslley Higor Campos Ferreira <weslley.ferreira@grupoboticario.com.br>
2025-01-06 22:36:55 -0300

    deactivated projects script 2

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,12 +1,12 @@
 import { createCardSkills } from "./skills.js";
 import { createCardExperiences } from "./experiences.js";
-import { createCardProject } from "./projects.js";
+// import { createCardProject } from "./projects.js";
 
 import { showOptionsChannelList } from "./contacts.js"
 
 createCardSkills();
 createCardExperiences();
-createCardProject();
+// createCardProject();
 showOptionsChannelList();
 
 //const app = document.querySelector('#app')
