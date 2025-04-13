0c16841dfde4a0676aa8a63132f8f39dce48550e
Weslley Higor <89808851+weslleyhigor@users.noreply.github.com>
2025-04-13 09:27:30 -0500

    Update skills.js

--- src/javascript/skills.js
+++ src/javascript/skills.js
@@ -2,7 +2,7 @@ import { skillsFrontend, skillsBackend, skillsLowCode } from "../database/data.j
 
 const cardFrontend = document.querySelector('.skill__frontend__card__container')
 const cardBackend = document.querySelector('.skill__backend__card__container')
-const cardLowcode = document.querySelector('.skill__lowcode__card__container')
+// const cardLowcode = document.querySelector('.skill__lowcode__card__container')
 
 export function createCardSkills () {
 
@@ -24,12 +24,12 @@ export function createCardSkills () {
         `)
     })
 
-    skillsLowCode.forEach((element)=>{
-        cardLowcode.insertAdjacentHTML("beforeend", `
-            <li class="skill__lowcode__card">
-                <img src="${element.logoUrl}">
-                <p>${element.name}</p>
-            </li>
-        `)
-    })
+    // skillsLowCode.forEach((element)=>{
+    //     cardLowcode.insertAdjacentHTML("beforeend", `
+    //         <li class="skill__lowcode__card">
+    //             <img src="${element.logoUrl}">
+    //             <p>${element.name}</p>
+    //         </li>
+    //     `)
+    // })
 }
